498a013c08c3314d8fcb9721e2ae867cdd8dcd5b
Zakki Mudhoffar <zakki.mudhoffar@gmail.com>
2022-05-17 09:47:20 +0700

    lom kelar

---
+++ quint/view/HomeView.swift
@@ -0,0 +1,37 @@
+//
+//  HomeView.swift
+//  quint
+//
+//  Created by Zakki Mudhoffar on 15/05/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ZStack{
+            RoundedRectangle(cornerRadius: 22)
+                .stroke(.black)
+                .padding(.bottom)
+                .frame(width: 356, height: 154)
+            
+            HStack {
+                VStack{
+                    Text("Lorem Ipsum dolor")
+                        .font(.system(size: 10))
+                    Text("Lorem ipsum dolor sit amet, consectetur")
+                        .font(.system(size: 8))
+                    Text("adipiscing elit, sed do eiusmod tempor")
+                        .font(.system(size: 8))
+                }
+                
+            }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
