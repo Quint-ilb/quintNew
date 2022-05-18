6c3d8558d114be0521152fa845c78ab2acda7b24
Zakki Mudhoffar <zakki.mudhoffar@gmail.com>
2022-05-18 12:31:09 +0700

    asd

---
+++ quint/view/MusicalNotes.swift
@@ -0,0 +1,20 @@
+//
+//  MusicalNotes.swift
+//  quint
+//
+//  Created by Zakki Mudhoffar on 18/05/22.
+//
+
+import SwiftUI
+
+struct MusicalNotes: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MusicalNotes_Previews: PreviewProvider {
+    static var previews: some View {
+        MusicalNotes()
+    }
+}
